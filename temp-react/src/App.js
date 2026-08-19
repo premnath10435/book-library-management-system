ac73fe7f647d37f8010b32099dc4dac27bd00f3d
premnath <premnath10435@gmail.com>
2026-08-19 18:44:21 +0530

    Fix React Router configuration

---
+++ temp-react/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import M from "./bookrouter";
+
+function App() {
+  return (
+    <>
+      <M />
+    </>
+  );
+}
+
+export default App;
