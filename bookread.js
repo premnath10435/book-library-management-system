b2952a9001a73eefe125c1a8aaef81ded440ddab
premnath10435 <premnath10435@gmail.com>
2026-08-22 13:09:15 +0530

    Update bookread.js

--- bookread.js
+++ bookread.js
@@ -9,18 +9,18 @@ function BookRead() {
     function getBook() {
 
         axios.get(
-    `https://book-library-management-system-backend-roj6.onrender.com/api/read/${id}`
-)
+            `https://book-library-management-system-backend-roj6.onrender.com/api/read/${id}`
+        )
 
-            .then((response) => {
-                console.log(response.data);
-                setBook(response.data);
-            })
+        .then((response) => {
+            console.log(response.data);
+            setBook(response.data);
+        })
 
-            .catch((error) => {
-                console.log(error);
-                alert("Book Not Found");
-            });
+        .catch((error) => {
+            console.log(error);
+            alert("Book Not Found");
+        });
     }
 
     return (
